64de7e665198a5e4e86e77324f4888296d3bf5e1
Claudia Harris <claudiavharris@gmail.com>
2025-07-10 11:25:35 -0400

    thursday before lunch

---
+++ week-1/StringCalculatorKata-main - thurs/StringCalculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        List<char> delimiters = [',', '\n'];
+
+        if (numbers == "")
+        {
+            return 0;
+                           
+        }
+            
+        if (numbers.StartsWith("//"))
+        {
+            var delimiter = numbers[2];
+            delimiters.Add(delimiter);
+            numbers = numbers[4..];
+        }
+
+        return numbers.Split([.. delimiters]) //string[]
+
+           
+                .Select(int.Parse) //makes int array
+                .Sum();
+
+    }
+}
---
+++ week-1/StringCalculatorKata-main - thurs/StringCalculator/CalculatorTests.cs
@@ -0,0 +1,65 @@
+﻿
+
+namespace StringCalculator;
+public class CalculatorTests
+    
+{
+    private Calculator _calculator = new();
+
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        
+
+        var result = _calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1",1)]
+    [InlineData("2",2)]
+    [InlineData("23",23)]
+    public void CanAddSingleInteger(string numbers, int expected)
+    {
+        
+        var result = _calculator.Add(numbers);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("2,2", 4)]
+    [InlineData("12,10", 22)]
+
+    public void CanAddTwoNumbers(string numbers, int expected)
+    {
+        
+        var result = _calculator.Add(numbers);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1\n2", 3)]
+
+    public void NewLineDelimiter(string numbers, int expected)
+    {
+
+        var result = _calculator.Add(numbers);
+        Assert.Equal(expected, result);
+    }
+
+
+    [Theory]
+    [InlineData("//;\n1;2", 3)]
+    [InlineData("//;\n1;4", 5)]
+    [InlineData("//*\n1*2", 3)]
+
+    public void CustomDelimiters(string numbers, int expected)
+    {
+
+        var result = _calculator.Add(numbers);
+        Assert.Equal(expected, result);
+    }
+}
+ 
